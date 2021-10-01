5b45e4857f3470f28c90a035ef99ff10f58a0191
mnzlucas <mnzlucas7@gmail.com>
2021-10-01 17:52:05 -0300

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,27 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from './home.module.scss';
 
- function Home() {
+import avatarImg from '../../public/images/avatar.svg';
+function Home() {
   return (
     <>
-      <Head>
+      <Head >
         <title>Início | ig.news</title>
       </Head>
-      <h1>Hello World</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for 9.90 month</span>
+          </p>
+        </section>
+        <Image src={avatarImg} alt="A girl coding" />
+      </main>
     </>
   )
 }
