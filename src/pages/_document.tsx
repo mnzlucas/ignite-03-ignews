f96cbc1ce47bdf6aa0d57e62a2ac6edde2591fb3
mnzlucas <mnzlucas7@gmail.com>
2021-09-30 22:15:02 -0300

    header component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,12 +7,14 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
-            </Head>
-            <body>
-        <Main />
-       <NextScript />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+          
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
       </body>
     </Html>
     )
