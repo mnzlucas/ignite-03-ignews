b9c2b4da0b8de6c8831457b37a81a5e2369672c4
mnzlucas <mnzlucas7@gmail.com>
2021-09-30 21:32:15 -0300

    app sctructure and styles completed

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+ function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default Home
