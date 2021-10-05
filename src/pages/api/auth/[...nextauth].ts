6b83bcf2945214cc4bda172ef8c5d604d232bdf9
mnzlucas <mnzlucas7@gmail.com>
2021-10-05 14:01:11 -0300

    using faunadb to save users

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query } from 'faunadb'
+
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -9,4 +13,41 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNINKEY,
+  },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+
+      try {
+
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index('user_by_email'),
+                  query.Casefold(user.email)
+                )
+              )
+            ),
+            query.Create(
+              query.Collection('users'),
+              { data: { email } }
+            ),
+            query.Get(
+              query.Match(
+                query.Index('user_by_email'),
+                query.Casefold(user.email)
+              )
+            )
+          ) 
+        )
+        return true
+      } catch (error) {
+        return false
+      }
+    },
+  }
 })
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { SubscribeButton } from '../components/SubscribeButton';
 import { GetStaticProps } from 'next';
-import { stripe } from '../service/stripe';
+import { stripe } from '../services/stripe';
 import Head from 'next/head';
 import Image from 'next/image';
 
